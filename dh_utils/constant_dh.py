f3442009f54ecaa09c1cd86998676d0e719ddcd2
raphaelseo22 <seodh02@gmail.com>
2024-01-21 20:36:40 +0900

    feat: Delete the constants file
    기존에 constant 파일에 있던 변수들을 각각 활용되는 파일에 명시

--- dh_utils/constant_dh.py
+++
@@ -1,6 +0,0 @@
-RCPNO_REGEX: str = r'''node[0-9]\['rcpNo'\]\s*=\s*"[0-9]*"'''
-DCMNO_REGEX: str = r'''node[0-9]\['dcmNo'\]\s*=\s*"[0-9]*"'''
-ELEID_REGEX: str = r'''node[0-9]\['eleId'\]\s*=\s*"[0-9]*"'''
-
-OG_URL = "https://dart.fss.or.kr/dsaf001/main.do?rcpNo={rcpno}"
-SRC_URL = "https://dart.fss.or.kr/report/viewer.do?rcpNo={rcpno}&dcmNo={dcmNo}&eleId={id}&offset=800&length=4053&dtd=dart3.xsd"
